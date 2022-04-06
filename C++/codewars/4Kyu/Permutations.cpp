52dbb66d2753071827106f26cb8558d63b85c4e5
KJHJason <kuanjunhaojason@gmail.com>
2022-04-06 22:43:12 +0800

    add

---
+++ C++/codewars/4Kyu/Permutations.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> permutations(std::string s)
+{
+    std::vector<std::string> ans;
+    std::sort(s.begin(), s.end());
+    do {
+        ans.push_back(s);
+    } while (std::next_permutation(s.begin(), s.end()));
+    return ans;
+}
+
+int main()
+{
+    std::vector<std::string> ans = permutations("aabb");
+    std::cout << "Answers:\n";
+    for (auto &s : ans) {
+        std::cout << s << std::endl;
+    }
+}
