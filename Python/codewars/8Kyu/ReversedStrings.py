f6c7c32db2a479aa7dd612853e33818b87e3aef4
KJHJason <kuanjunhaojason@gmail.com>
2022-04-16 16:17:17 +0800

    edit

--- Python/codewars/8Kyu/ReversedStrings.py
+++ Python/codewars/8Kyu/ReversedStrings.py
@@ -1,3 +1,3 @@
 # https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
 def solution(string):
-    return "".join([c for c in reversed(string)])
+    return "".join([c for c in reversed(string)]) # or string[::-1]
