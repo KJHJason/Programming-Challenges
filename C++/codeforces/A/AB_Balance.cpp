47e483767d4595d648179cda4e6954d1a675c26f
KJHJason <kuanjunhaojason@gmail.com>
2022-05-07 12:35:35 +0800

    edit

--- C++/codeforces/A/AB_Balance.cpp
+++ C++/codeforces/A/AB_Balance.cpp
@@ -1,58 +1,96 @@
-// https://codeforces.com/problemset/problem/1606/A
+//
+//             ____            _                   _      
+//            |  _ \ _ __ __ _| |_ ___  _ __ _ __ (_) ___ 
+//            | | | | '__/ _` | __/ _ \| '__| '_ \| |/ __|
+//            | |_| | | | (_| | || (_) | |  | | | | | (__ 
+//            |____/|_|  \__,_|\__\___/|_|  |_| |_|_|\___|
+//
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⡤⠤⠤⣀⣀⣀⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣤⢔⢒⢓⢶⣶⣦⡙⢿⣿⡿⣿⢻⢛⠖⡒⢤⢤⣄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⣤⣶⣿⠫⡂⠆⡕⣼⣿⣻⣽⣿⣼⣿⢑⢑⠌⡢⢑⢌⠢⡊⢌⣟⣷⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⣀⣀⠄⠄⣀⣤⣤⣴⣿⣿⡿⠧⠑⠚⠚⠛⠛⠚⠛⠛⠛⠹⠾⢵⣅⡕⡨⡂⣦⡷⣾⣯⣿⣽⣟⣿⣷⣦⡀⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠈⢳⡫⡷⣾⡫⣎⡾⠚⠋⠉⠄⠄⡤⠐⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠙⢒⣿⣿⣻⡿⣾⢷⡿⣽⡷⣿⣽⣿⣦⡀⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⣞⣿⢜⢮⠟⠋⠄⠄⠄⠄⣤⠞⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⠄⠠⡄⢯⠉⡿⢻⢟⡟⡟⡿⣿⡻⠫⠣⡙⠅⠷⣄⠄⠄⠄
+// ⠄⠄⠄⠄⣼⢕⡿⡽⠋⠄⠄⠄⢀⣠⠞⠏⠄⠤⠄⠄⠄⠄⠄⠂⠈⡀⠄⢰⣇⠄⣮⠊⠱⠘⠻⡕⡝⡪⡹⡜⡟⣧⡕⡨⡘⢌⢚⣄⠄⠄
+// ⠄⠄⠄⢸⣗⡯⣺⠁⠄⠄⠄⣴⢣⠋⢸⠄⢀⠄⠄⠄⡀⠈⠄⠄⡠⠐⠄⡟⣿⠄⡿⠶⠶⠋⡂⢻⣮⣻⡮⣷⡳⡫⡿⡐⢌⢂⠢⣻⡄⠄
+// ⠄⠄⢀⣤⡿⣛⢿⢀⠄⠄⣰⠛⠛⠄⠘⣄⠸⡀⠄⡄⠄⠄⢀⣴⠃⣠⠜⠄⣿⢠⡇⡶⠊⠙⠋⠄⣿⠯⣝⣿⢵⢵⡹⣧⡱⣰⡵⣦⣿⠄
+// ⠄⠄⣴⠋⢅⠊⢎⢷⡃⣼⡛⠄⣀⣀⣀⡙⠓⠙⠂⠳⠤⠐⠋⠃⠚⠁⠄⠄⠯⠚⢀⠃⡀⠄⢀⠄⣿⡽⡼⡽⣗⡗⣏⣿⣷⢿⣻⣟⣿⠄
+// ⠄⠐⣯⠨⢂⠅⠅⢽⣇⠇⠐⠛⠋⣉⣉⣛⠛⠄⠄⠄⠄⠄⠄⢀⣤⠤⠤⠤⣤⣄⡘⠾⡤⢚⡌⣤⢿⡺⡜⡮⣯⢻⣼⣿⢾⣿⢿⣽⢿⡇
+// ⠄⣤⢟⠪⢐⠌⣼⠿⠼⠄⠄⠐⠯⠦⠵⠬⠩⠆⠄⠄⠄⠄⠄⠄⡠⡠⠠⡠⠠⣉⠛⠄⣿⠘⣾⠣⢓⠻⡳⢿⡷⠻⣿⣽⣿⣻⣿⣽⢿⠃
+// ⠄⣏⡂⡊⠔⡨⢻⣀⠄⠂⡁⠂⠄⠄⠄⠄⠄⠄⠄⠠⡀⠂⠁⠄⠈⠁⠓⠚⠚⠘⠄⠄⣿⠄⢻⣈⠢⠨⣂⣽⡗⡣⠩⡂⢍⢓⢛⠜⣻⠄
+// ⠄⢷⣕⡎⡖⣜⣲⠟⠛⠢⠤⣦⣈⣀⡀⠄⢀⠠⠄⠄⠄⠄⠄⠄⠄⠄⠂⡁⠄⠁⠌⢀⡟⠄⡴⡩⠩⡘⠫⣣⡌⢬⢊⢌⢂⠆⡢⣱⠟⠄
+// ⠄⣴⠫⠨⡊⠌⢌⣧⠑⢣⠘⠜⠤⣍⠙⠛⠒⠒⠒⠂⠤⠥⠤⠬⠤⠤⠤⠤⠤⠥⠤⣾⠃⡀⢷⣌⢆⢎⢬⣢⣧⣬⣧⣷⣴⣵⣬⠋⠄⠄
+// ⠄⣯⣽⣼⣴⣽⣱⣹⡆⡜⠄⢥⣃⢿⣶⣤⣤⣀⣤⣀⣄⠄⠄⠄⠄⢀⠄⠄⢀⠄⢴⣃⡶⣥⠞⢍⠑⢍⢊⠻⣿⣟⣯⣿⣾⠟⠃⠄⠄⠄
+// ⠄⠉⠁⠄⠙⠁⠙⠛⢠⠄⢀⠄⠙⠺⣿⣻⣟⣿⢿⡢⡑⢝⢿⣿⣿⣿⣿⣿⣯⡙⢅⠣⣩⢯⢪⢔⠥⡅⡆⡇⡟⣿⡯⠛⠁⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⢠⠋⠄⢸⠄⠐⠕⢄⠛⢛⣿⢿⣷⣬⢂⠢⠺⣷⢿⡾⣯⣿⣧⣥⣕⣜⢓⢛⣷⣽⣜⣽⢟⣟⠟⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⢻⡠⠄⠸⠄⠄⠙⠊⠂⠒⢻⣄⣈⠍⠟⢟⠟⣶⠻⢟⣿⣗⣟⣛⣛⣛⣛⡫⠙⠷⣝⣮⠗⠛⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠓⠤⣄⣃⠄⠄⠄⠄⠄⠄⠈⠓⠒⠄⠐⠚⠁⠄⠄⠄⠘⠠⠤⠤⠤⠚⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+//
+// https://codeforces.com/contest/1606/problem/A
 
 #include <bits/stdc++.h>
 using namespace std;
-#define br "\n";
 
-string getABorBA(string in, int i) {
-    string ss;
-    ss += in[i];
-    ss += in[i+1];
-    return ss;
+typedef int_fast32_t fi32;
+typedef int_fast64_t fi64;
+typedef uint_fast32_t ufi32;
+typedef uint_fast64_t ufi64;
+
+typedef vector<int> vi;
+typedef vector<fi64> vfi64;
+typedef vector<ufi64> vufi64;
+typedef pair<int, int> pii;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<fi64, fi64> umfi64;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<string, fi64> umsfi64;
+typedef map<int, int> mii;
+typedef map<fi64, fi64> mfifi;
+typedef map<string, int> msi;
+typedef map<string, fi64> msfi;
+
+typedef long double ld;
+typedef unsigned int ui;
+typedef long int li;
+typedef unsigned long int uli;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define inpToVec(v) for (auto& i : v) cin >> i
+#define sortA(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.begin(), v.end(), greater{})
+#define prtArr(x) for (const auto& n : x) cout << n << " "; cout << "\n"
+#define prtMap(m) for (const auto& [key, value] : m) cout << key << " " << value << "\n"
+#define prtY cout << "YES\n"
+#define prtN cout << "NO\n"
+#define prtYN(b) cout << (b ? "YES" : "NO") << "\n"
+#define prtAns(ans) cout << ans << "\n"
+
+const fi64 MOD {998244353};
+
+void solve() 
+{
+    string s; cin >> s;
+    prtAns(s.back() + s.substr(1));
 }
 
-tuple <int, int> getNumOfABorBA(string in) {
-    int ab = 0;
-    int ba = 0;
-    int strlen = in.length();
-    for (int i = 0; i < strlen; i++) {
-        string ss = getABorBA(in, i);
-        if (ss == "ab") {
-            ab++;
-        } else if (ss == "ba") {
-            ba++;
-        }
-    }
-    return {ab, ba};
-}
-
-int main() {
-    int t; cin >> t;
-    while (t--) {
-        string in; cin >> in;
-        int ab = 0, ba = 0;
-        int strlen = in.length();
-        if (strlen > 1) {
-            tie(ab, ba) = getNumOfABorBA(in);
-            // cout << "Num of ab: " << ab << br;
-            // cout << "Num of ba: " << ba << br;
-            if (ab > ba) {
-                for (int i = strlen; i > 0; i--) {
-                    in.replace(i-1, 1, "a");
-                    tie(ab, ba) = getNumOfABorBA(in);
-                    if (ab == ba) break;
-                }
-            } else if (ba > ab) {
-                for (int i = strlen; i > 0; i--) {
-                    in.replace(i-1, 1, "b");
-                    tie(ab, ba) = getNumOfABorBA(in);
-                    if (ab == ba) break;
-                }
-            }
-            cout << in << br;
-        } else {
-            cout << in << br;
-        }
+int main() 
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int t = 1;
+    bool haveTestCases = 1; // change accordingly
+    if (haveTestCases) cin >> t;
+    for (int i = 0; i < t; ++i) {
+        solve();
     }
     return 0;
 }
